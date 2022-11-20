087ff617ad12affd8a2256eb31db3488caf5aad2
AlexDstart <demyanalex@gmail.com>
2022-11-20 22:18:07 +0300

    задание 3

--- src/Main.java
+++ src/Main.java
@@ -15,8 +15,29 @@ public class Main {
         if (age >= 18 && age < 24) {
             System.out.println(" Человек ходит в университет ");
         }
-        if (age>=24){
+        if (age >= 24) {
             System.out.println(" Человек ходит на работу ");
+        }
+
+
+        int cappacity = 102;
+        int seatsCount = 62;
+        int standingCount = cappacity - seatsCount;
+        int seatsUsed = 62;
+        int standingUsed = 40;
+        if (seatsUsed < seatsCount) {
+
+            System.out.println(" Есть еще " + (seatsCount - seatsUsed) + " сидячих мест ");
+        }
+        if (standingUsed < standingCount) {
+
+            System.out.println(" Есть еще " + (standingCount - standingUsed) + " Стоячих мест ");
+        }
+        if (standingUsed == standingCount) {
+            System.out.println(" Стоячих мест нет ");
+        }
+        if (seatsUsed == seatsCount) {
+            System.out.println(" Сидячих мест нет ");
         }
     }
 }
