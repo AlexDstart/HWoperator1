752ea209aa0ef8fbcb7bb94ff62568169ee1b99e
AlexDstart <demyanalex@gmail.com>
2022-11-20 21:26:22 +0300

    задание 1

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(" Задание 1 ");
+        int age = 17;
+        if (age>=18){
+            System.out.println(" Поздравляем с совершеннолетием ");
+        }
+        if (age<18){
+            System.out.println(" Взраст соверщеннолетия еще не наступил нужно подождать ");
+        }
+    }
+}
